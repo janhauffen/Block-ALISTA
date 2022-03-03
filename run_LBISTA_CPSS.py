d7d6b7c3c204102db7ed2bab8e19d16840a52ecd
Jan Hauffen <62329402+janhauffen@users.noreply.github.com>
2022-03-03 09:38:32 +0100

    Update run_LBISTA_CPSS.py

--- run_LBISTA_CPSS.py
+++ run_LBISTA_CPSS.py
@@ -27,23 +27,17 @@ SNR = SNR_list[k]
 MC = 250
 prob = problem.block_gaussian_trial(m=50, L=15, B=5, MC=MC, pnz=0.1, SNR_dB=SNR)  # a Block-Gaussian x, noisily observed through a random matrix
 # pdb.set_trace()
-# layers, W = network.build_BALISTA(prob,T=T,initial_lambda=0.1, initial_gamma=2/(1.01*la.norm(prob.A,2)**2), untied=False)
-layers = network.build_LBISTA_CPSS(prob,T=T,initial_lambda=0.1, initial_gamma=0.1, untied=False)
-#layers = network.build_TiBLISTA(prob,T=T,initial_lambda=0.1, initial_gamma=1, untied=False)
+layers = network.build_LBISTA_CPSS(prob,T=T,initial_lambda=0.1, initial_gamma=0.1)
 # pdb.set_trace()
 start = time.time()
-training_stages = train.setup_training(layers,prob,trinit=1e-3,refinements=(0.5, 0.01, 0.001))
+training_stages = train.setup_training(layers,prob,trinit=1e-3)
 end = time.time()
 print( 'Took me {totaltime:.3f} minutes for setup training'.format(totaltime = (end-start)/60))
 
-# sess = train.do_training(training_stages,prob,'TiBLISTA_block_Gauss_giidT12Thermo6-Jan.npz')
 sess = train.do_training(training_stages,prob,'trainings/LBISTA_CPSS_T'+str(T)+'_SNRdB_'+str(SNR)+'batch'+str(MC)+'.npz')
 """# Evaluating"""
-# sparsemat = sio.loadmat('mat/normalized_D_blocktestSNR' + str(SNR) + '.mat')
-# y = sparsemat.get('y')
-# x = sparsemat.get('x')
+
 #y,x = prob(sess)
-#sio.savemat('mat/blocktestSNR'+str(SNR)+'.mat', {'y': y, 'x':x, 'D': prob.A})
 sparsemat = sio.loadmat('mat/small_normalized_D_blocktestSNR' + str(SNR) + '.mat')
 y = sparsemat.get('y')
 x = sparsemat.get('x')
