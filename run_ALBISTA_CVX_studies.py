a778b734fb950ee8fea86f330c4fdbce6c339b79
Jan Hauffen <62329402+janhauffen@users.noreply.github.com>
2022-03-03 09:36:46 +0100

    Update run_ALBISTA_CVX_studies.py

--- run_ALBISTA_CVX_studies.py
+++ run_ALBISTA_CVX_studies.py
@@ -25,27 +25,23 @@ tf.set_random_seed(1) # on the other hand, this is cbasically useless (see issue
 SNR_list=[np.infty,40, 20, 10]
 k = 1
 T=16
-# SNR = np.inf #wird SNR_dB = -10 mein Gegenbeispiel?
+
 SNR = SNR_list[k]
 MC = 250
 prob = problem.block_gaussian_trial(m=50, L=15, B=5, MC=MC, pnz=0.1, SNR_dB=SNR) # a Block-Gaussian x, noisily observed through a random matrix
 # pdb.set_trace()
-# layers, W = network.build_BALISTA(prob,T=T,initial_lambda=0.1, initial_gamma=2/(1.01*la.norm(prob.A,2)**2), untied=False)
-layers, W = network.build_BALISTA_v5(prob,T=T,initial_lambda=0.5, initial_gamma=0.1, untied=False)
-#layers = network.build_TiBLISTA(prob,T=T,initial_lambda=0.1, initial_gamma=1, untied=False)
+
+layers, W = network.build_BALISTA_v5(prob,T=T,initial_lambda=0.5, initial_gamma=0.1)
+
 # pdb.set_trace()
 start = time.time()
-training_stages = train.setup_training(layers,prob,trinit=1e-3,refinements=(0.5, 0.01, 0.001))
+training_stages = train.setup_training(layers,prob,trinit=1e-3)
 end = time.time()
 print( 'Took me {totaltime:.3f} minutes for setup training'.format(totaltime = (end-start)/60))
 
-# sess = train.do_training(training_stages,prob,'TiBLISTA_block_Gauss_giidT12Thermo6-Jan.npz')
 sess = train.do_training(training_stages,prob,'trainings/batch250_D_ALBISTA_CVX_v4_block_Gauss_giidT'+str(T)+'Thermo6-JanWithSNR_dB'+str(SNR)+'.npz')
 """# Evaluating"""
-# sparsemat = sio.loadmat('blocktest.mat')
-# y = sparsemat.get('y')
-# x = sparsemat.get('x')
-# y,x = prob(sess)
+
 sparsemat = sio.loadmat('mat/sparse_case_D_blocktestSNR' + str(SNR) + '.mat')
 y = sparsemat.get('y')
 x = sparsemat.get('x')
